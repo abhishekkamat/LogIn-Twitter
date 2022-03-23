1c8221098345082c9ec7302a8041a77cab596e42
abhishekkamat <96072575+abhishekkamat@users.noreply.github.com>
2022-03-24 02:37:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function myFunction(){
   xhr.setRequestHeader("Authorization", "OAuth oauth_consumer_key="+API_key+", oauth_nonce="+authnonce+", oauth_signature=oauth_signature, oauth_signature_method=HMAC-SHA1, oauth_timestamp="+timeStamp+", oauth_version=1.0");
   xhr.setRequestHeader("Access-Control-Allow-Origin","*");
   xhr.setRequestHeader("Origin-Agent-Cluster","?0");
-  document.domain="abhishekkamat.github.io";
+  document.domain="abhishekkamat.github.io/LogIn-Twitter/";
   xhr.send();
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
